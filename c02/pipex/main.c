7f0f121e12bfba92ab7b73bee3d445c89eb622e2
John Gamboa Alegria <jgamboa-@c2r2s2.42lausanne.ch>
2023-03-06 16:13:08 +0100

    pippiex begin

--- c02/pipex/main.c
+++ c02/pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: jgamboa- <jgamboa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 16:19:00 by jgamboa-          #+#    #+#             */
-/*   Updated: 2023/02/28 16:33:44 by jgamboa-         ###   ########.fr       */
+/*   Updated: 2023/03/06 16:10:54 by jgamboa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 int main (void)
 {
-	test_print();
-
+	int id = fork();
+	p(1 SS "Hello World from " II (id) NL);
+	if (id == 0)
+		p(1 SS "Hello from the new process" NL);
+	else
+	{
+		p(1 SS "Hello from the main process" NL);
+	}
+	
 	return (0);
 }
