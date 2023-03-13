3253d999b8f9649259e83cdced6cc810eab8d973
John Gamboa Alegria <jgamboa-@c2r2s7.42lausanne.ch>
2023-03-13 14:53:31 +0100

    tests

--- c02/pipex/main.c
+++ c02/pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: jgamboa- <jgamboa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 16:19:00 by jgamboa-          #+#    #+#             */
-/*   Updated: 2023/03/10 17:30:38 by jgamboa-         ###   ########.fr       */
+/*   Updated: 2023/03/13 14:50:28 by jgamboa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,17 @@
 
 int main (void)
 {
-	int id = fork();
-	int n;
+	int fd[2];
 
-	if (id == 0)
-		n = 1;
-	else
-		n = 6;
-	if (id != 0)
-		wait(&id);
-	for (int i = n; i < n + 5; i++)
-	{
-		ft_printf("Current ID: %d, parent %d", getpid);
-		fflush(stdout);
+	pipe(fd);
+	if (fd == -1){
+		printf("Error\n");
+		return (1);
 	}
-	p(1 NL);
+	int id = fork();
+	if (id == 0) {
+		int x;
+		printf("");
+	}
 	return (0);
 }
