9e6c18ba64fe0eda859fe1e08e4ccbf5c1433581
johng <jgamboa.a@proton.me>
2023-03-16 17:07:44 +0100

    Learning 1st steps

--- c02/pipex/main.c
+++ c02/pipex/main.c
@@ -3,28 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgamboa- <jgamboa-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jgamboa- <jgamboa-@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 16:19:00 by jgamboa-          #+#    #+#             */
-/*   Updated: 2023/03/13 14:50:28 by jgamboa-         ###   ########.fr       */
+/*   Updated: 2023/03/16 17:03:00 by jgamboa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int main (void)
+int main (int argc, char **argv)
 {
-	int fd[2];
 
-	pipe(fd);
-	if (fd == -1){
-		printf("Error\n");
-		return (1);
-	}
-	int id = fork();
-	if (id == 0) {
-		int x;
-		printf("");
+	char	*command_path;
+	char	*arguments[3];
+	char	**commands1;
+	char	*file1;
+
+	file1 = argv + 1;
+
+	commands1 = ft_split(argv + 2, ' ');
+	command_path = ft_strcat("/bin/";
+	arguments[0] = command_path;
+	arguments[1] = "-la";
+	arguments[2] = NULL;
+	if (argc  == 5)
+	if (argc > 2)
+	{
+
+		p(1 SS (argv[1]) NL);
+		execve(command_path, arguments, NULL);
+		printf("OK!");
 	}
 	return (0);
 }
