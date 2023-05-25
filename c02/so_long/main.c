ed9ad0e70d6b0f81c940dc7914defac4b6003236
John GAMBOA <jgamboa-@student.42lausanne.ch>
2023-05-25 05:50:28 +0200

    fix compilation of mlx lib

--- c02/so_long/main.c
+++ c02/so_long/main.c
@@ -3,22 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgamboa- <jgamboa-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jgamboa- <jgamboa-@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 16:34:50 by jgamboa-          #+#    #+#             */
-/*   Updated: 2023/05/23 16:21:58 by jgamboa-         ###   ########.fr       */
+/*   Updated: 2023/05/25 05:42:51 by jgamboa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include "mlx.h"
 
 
 // check args
 // check extension fichier .ber |||| example map.ber \\ a.ber
 // check la map si elle est rectangulaire
 
-int	main()
+int	main(void)
 {
-	ft_printchar('f');
-	return (0);
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
